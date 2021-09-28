26ea4796ade20cac282cac93bd127c11b655abc8
rmnsk87 <45914190+rmnsk87@users.noreply.github.com>
2021-09-29 01:39:43 +0300

    Update product_carousel.js
    Added caption, price, and button "Add to cart" to slides

--- js/product_carousel.js
+++ js/product_carousel.js
@@ -1,9 +1,9 @@
 (function () {
     const slides = [
-        '<img src="products/red_guitar_telecaster-min.png" alt="Гітара Telecaster">',
-        '<img src="products/black_guitar_stratocaster-min.png" alt="Гітара Startocaster">',
-        '<img src="products/black_guitar_les_paul-min.png" alt="Чорний Les Paul">',
-        '<img src="products/acoustic_guitar-min.png" alt="Акустична гітара">',
+        '<div><img src="products/red_guitar_telecaster-min.png" alt="Гітара Telecaster"><h3 class="article">Гітара модель 1</h3><p class="newproducts_price">8000 грн.</p><p class="newproducts_btn"><button type="button" class="btn btn-default" data-toggle="modal" data-target="#ModalWindowBasketcase">В кошик</button></p></div>',
+        '<div><img src="products/black_guitar_stratocaster-min.png" alt="Гітара Startocaster"><h3 class="article">Гітара модель 2</h3><p class="newproducts_price">6 000 грн.</p><p class="newproducts_btn"><button type="button" class="btn btn-default" data-toggle="modal" data-target="#ModalWindowBasketcase">В кошик</button></p></div>',
+        '<div><img src="products/black_guitar_les_paul-min.png" alt="Чорний Les Paul"><h3 class="article">Гітара модель 4</h3><p class="newproducts_price">10 000 грн.</p><p class="newproducts_btn"><button type="button" class="btn btn-default" data-toggle="modal" data-target="#ModalWindowBasketcase">В кошик</button></p></div>',
+        '<div><img src="products/acoustic_guitar-min.png" alt="Акустична гітара"><h3 class="article">Гітара модель 3</h3><p class="newproducts_price">4 200 грн.</p><p class="newproducts_btn"><button type="button" class="btn btn-default" data-toggle="modal" data-target="#ModalWindowBasketcase">В кошик</button></p></div>',
     ];
 
     let currentSlide = 0;
