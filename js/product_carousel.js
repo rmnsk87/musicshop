5655f01070b5a3cdeae1f22d989a27725c413d23
rmnsk87 <45914190+rmnsk87@users.noreply.github.com>
2021-09-29 12:43:49 +0300

    Update product_carousel.js

--- js/product_carousel.js
+++ js/product_carousel.js
@@ -1,9 +1,9 @@
 (function () {
     const slides = [
-        '<div><img src="products/red_guitar_telecaster-min.png" alt="Гітара Telecaster"><h3 class="article">Гітара модель 1</h3><p class="newproducts_price">8000 грн.</p><p class="newproducts_btn"><button type="button" class="btn btn-default"        data-toggle="modal" data-target="#ModalWindowBasketcase">В кошик</button></p></div>',
-        '<div><img src="products/black_guitar_stratocaster-min.png" alt="Гітара Startocaster"><h3 class="article">Гітара модель 2</h3>        <p class="newproducts_price">6 000 грн.</p><p class="newproducts_btn"><button type="button" class="btn btn-default"        data-toggle="modal" data-target="#ModalWindowBasketcase">В кошик</button></p></div>',
-        '<div><img src="products/black_guitar_les_paul-min.png" alt="Чорний Les Paul"><h3 class="article">Гітара модель 4</h3><p class="newproducts_price">10 000 грн.</p><p class="newproducts_btn"><button type="button" class="btn btn-default"        data-toggle="modal" data-target="#ModalWindowBasketcase">В кошик</button></p></div>',
-        '<div><img src="products/acoustic_guitar-min.png" alt="Акустична гітара"><h3 class="article">Гітара модель 3</h3><p class="newproducts_price">4 200 грн.</p><p class="newproducts_btn"><button type="button" class="btn btn-default"        data-toggle="modal" data-target="#ModalWindowBasketcase">В кошик</button></p></div>',
+        '<div><img src="products/red_guitar_telecaster-min.png" alt="Гітара Telecaster"><h3 class="article">Гітара модель 1</h3><p class="newproducts_price">8000 грн.</p><p class="newproducts_btn"><button type="button" class="btn btn-default" data-toggle="modal" data-target="#ModalWindowBasketcase">В кошик</button></p></div>',
+        '<div><img src="products/black_guitar_stratocaster-min.png" alt="Гітара Startocaster"><h3 class="article">Гітара модель 2</h3><p class="newproducts_price">6 000 грн.</p><p class="newproducts_btn"><button type="button" class="btn btn-default" data-toggle="modal" data-target="#ModalWindowBasketcase">В кошик</button></p></div>',
+        '<div><img src="products/black_guitar_les_paul-min.png" alt="Чорний Les Paul"><h3 class="article">Гітара модель 4</h3><p class="newproducts_price">10 000 грн.</p><p class="newproducts_btn"><button type="button" class="btn btn-default" data-toggle="modal" data-target="#ModalWindowBasketcase">В кошик</button></p></div>',
+        '<div><img src="products/acoustic_guitar-min.png" alt="Акустична гітара"><h3 class="article">Гітара модель 3</h3><p class="newproducts_price">4 200 грн.</p><p class="newproducts_btn"><button type="button" class="btn btn-default" data-toggle="modal" data-target="#ModalWindowBasketcase">В кошик</button></p></div>',
     ];
 
     let currentSlide = 0;
@@ -12,7 +12,7 @@
         const slideContainer = document.querySelector(".newproducts_container");
         let html = '';
 
-        if ((window.innerWidth < 607 && window.innerWidth > 330)) {
+        if ((window.innerWidth < 607)) {
 
             html = slides[currentSlide];
             const nextSlide = currentSlide + 1 < slides.length ? currentSlide + 1 : 0;
