4785f333774b6bfd86a709dce8b6cf734ae10545
rmnsk87 <45914190+rmnsk87@users.noreply.github.com>
2021-09-29 12:40:56 +0300

    Update product_carousel.js
    Fixed mobile responsive view of products. 2 products per 1 row

--- js/product_carousel.js
+++ js/product_carousel.js
@@ -1,9 +1,9 @@
 (function () {
     const slides = [
-        '<div><img src="products/red_guitar_telecaster-min.png" alt="Гітара Telecaster"><h3 class="article">Гітара модель 1</h3><p class="newproducts_price">8000 грн.</p><p class="newproducts_btn"><button type="button" class="btn btn-default" data-toggle="modal" data-target="#ModalWindowBasketcase">В кошик</button></p></div>',
-        '<div><img src="products/black_guitar_stratocaster-min.png" alt="Гітара Startocaster"><h3 class="article">Гітара модель 2</h3><p class="newproducts_price">6 000 грн.</p><p class="newproducts_btn"><button type="button" class="btn btn-default" data-toggle="modal" data-target="#ModalWindowBasketcase">В кошик</button></p></div>',
-        '<div><img src="products/black_guitar_les_paul-min.png" alt="Чорний Les Paul"><h3 class="article">Гітара модель 4</h3><p class="newproducts_price">10 000 грн.</p><p class="newproducts_btn"><button type="button" class="btn btn-default" data-toggle="modal" data-target="#ModalWindowBasketcase">В кошик</button></p></div>',
-        '<div><img src="products/acoustic_guitar-min.png" alt="Акустична гітара"><h3 class="article">Гітара модель 3</h3><p class="newproducts_price">4 200 грн.</p><p class="newproducts_btn"><button type="button" class="btn btn-default" data-toggle="modal" data-target="#ModalWindowBasketcase">В кошик</button></p></div>',
+        '<div><img src="products/red_guitar_telecaster-min.png" alt="Гітара Telecaster"><h3 class="article">Гітара модель 1</h3><p class="newproducts_price">8000 грн.</p><p class="newproducts_btn"><button type="button" class="btn btn-default"        data-toggle="modal" data-target="#ModalWindowBasketcase">В кошик</button></p></div>',
+        '<div><img src="products/black_guitar_stratocaster-min.png" alt="Гітара Startocaster"><h3 class="article">Гітара модель 2</h3>        <p class="newproducts_price">6 000 грн.</p><p class="newproducts_btn"><button type="button" class="btn btn-default"        data-toggle="modal" data-target="#ModalWindowBasketcase">В кошик</button></p></div>',
+        '<div><img src="products/black_guitar_les_paul-min.png" alt="Чорний Les Paul"><h3 class="article">Гітара модель 4</h3><p class="newproducts_price">10 000 грн.</p><p class="newproducts_btn"><button type="button" class="btn btn-default"        data-toggle="modal" data-target="#ModalWindowBasketcase">В кошик</button></p></div>',
+        '<div><img src="products/acoustic_guitar-min.png" alt="Акустична гітара"><h3 class="article">Гітара модель 3</h3><p class="newproducts_price">4 200 грн.</p><p class="newproducts_btn"><button type="button" class="btn btn-default"        data-toggle="modal" data-target="#ModalWindowBasketcase">В кошик</button></p></div>',
     ];
 
     let currentSlide = 0;
@@ -11,14 +11,24 @@
     function showCurrentSlide() {
         const slideContainer = document.querySelector(".newproducts_container");
         let html = '';
-        html = slides[currentSlide];
-        const nextSlide = currentSlide + 1 < slides.length ? currentSlide + 1 : 0;
-        html += slides[nextSlide];
-        const next2Slide = nextSlide + 1 < slides.length ? nextSlide + 1 : 0;
-        html += slides[next2Slide];
-        const next3Slide = next2Slide + 1 < slides.length ? next2Slide + 1 : 0;
-        html += slides[next3Slide];
-        slideContainer.innerHTML = html;
+
+        if ((window.innerWidth < 607 && window.innerWidth > 330)) {
+
+            html = slides[currentSlide];
+            const nextSlide = currentSlide + 1 < slides.length ? currentSlide + 1 : 0;
+            html += slides[nextSlide];
+            slideContainer.innerHTML = html;
+
+        } else {
+            html = slides[currentSlide];
+            const nextSlide = currentSlide + 1 < slides.length ? currentSlide + 1 : 0;
+            html += slides[nextSlide];
+            const next2Slide = nextSlide + 1 < slides.length ? nextSlide + 1 : 0;
+            html += slides[next2Slide];
+            const next3Slide = next2Slide + 1 < slides.length ? next2Slide + 1 : 0;
+            html += slides[next3Slide];
+            slideContainer.innerHTML = html;
+        }
     }
 
     function nextSlide() {
