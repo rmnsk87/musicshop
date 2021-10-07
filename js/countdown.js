bdd7a9b360c2ac362e2f0ca2c39b6ed8e246f8b9
rmnsk87 <45914190+rmnsk87@users.noreply.github.com>
2021-10-07 17:06:47 +0300

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -1,17 +1,21 @@
-const finalDate = new Date("Dec 31, 2021 23:59:59").getTime();
-let x = setInterval(function () {
+(function () {
+    const finalDate = new Date("Dec 31, 2021 23:59:59").getTime();
+    let x = setInterval(function () {
 
-    let now = new Date().getTime();
-    let difference = finalDate - now;
-    let days = Math.floor(difference / (1000 * 60 * 60 * 24));
-    let hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    let minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((difference % (1000 * 60)) / 1000);
+        let now = new Date().getTime();
+        let difference = finalDate - now;
+        let days = Math.floor(difference / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+        let seconds = Math.floor((difference % (1000 * 60)) / 1000);
 
-    document.getElementById("countdown_clock").innerHTML = `<div>Days:<br/> <span>${days}</span></div>` + `<div>Hours:<br/> <span>${hours}</span></div>` + `<div>Minutes:<br/> <span>${minutes}</span></div>` + `<div>Seconds:<br/> <span>${seconds}</span></div>`;
+        document.getElementById("countdown_clock").innerHTML = `<div>Days:<br/> <span>${days}</span></div>` + `<div>Hours:<br/> <span>${hours}</span></div>` + `<div>Minutes:<br/> <span>${minutes}</span></div>` + `<div>Seconds:<br/> <span>${seconds}</span></div>`;
 
-    if (difference < 0) {
-        clearInterval(x);
-        document.getElementById("countdown_clock").innerHTML = "School begins!";
-    }
-}, 1000);
+        if (difference < 0) {
+            clearInterval(x);
+            document.getElementById("countdown_clock").innerHTML = "School begins!";
+        }
+    }, 1000);
+
+})();
+
