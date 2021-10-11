c6c744447a5e498d3ab4ef4b2c2c4d35c34a0271
rmnsk87 <45914190+rmnsk87@users.noreply.github.com>
2021-10-11 11:40:23 +0300

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,26 +1,11 @@
 let clicks = 0;
-
-const guitarTele = document.getElementById("telecaster");
-const resultName = guitarTele.getAttribute("name");
-
 function increaseCartNumber() {
 	clicks += 1;
 	document.getElementById("clicks").innerHTML = ' ' + clicks;
 	document.getElementById("show-cart-qta").innerHTML = ' ';
-	document.getElementById("show-cart-list").innerHTML = '<div>' + resultName + '</div>' + '<div>' + clicks + ' pcs. </div>';
+	document.getElementById("show-cart-list").innerHTML = '<div> In Your cart: ' + '<b class="goldetex">' + clicks + '</b> guitar </div>';
 
 };
-
-
-
-
-
-
-
-
-
-
-
 
 
 
